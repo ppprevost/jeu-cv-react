631b135b4e6e3838a1c02cbd993f8290dc32aa19
Pierre-Philippe Prevost <ppprevost@aol.fr>
2019-12-14 21:40:05 +0100

    feat: addd background

--- jeu-cv-react/src/components/Background.tsx
+++ jeu-cv-react/src/components/Background.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import {RenderItem} from "../game-engine";
+import React, {useEffect} from 'react';
+import background1 from '../img/background/background1.png'
+import background2 from '../img/background/background2.png'
+import background3 from '../img/background/background3.png'
+import background4 from '../img/background/background4.png'
+import background5 from '../img/background/background5.png'
+import background6 from '../img/background/background6.png'
+import background7 from '../img/background/background7.png'
 
-const Background = () => {
-    return <img title={"test"} />
+const backgroundImg = [background1, background2, background3, background4, background5, background6, background7]
+
+const style = {
+    top: 470,
+    width: 90,
+    height: 80
 }
 
-export default RenderItem(Background)()()()
+const Background = ({left}: {left:number}) => {
+    const {top, width, height} = style;
+    const objetRandom = Math.round(Math.random() * 6) + 1;
+    const avatar = backgroundImg[objetRandom];
+    return <img className='background' src={avatar} style={{top, width, height,left, position:'absolute'}} />
+}
+
+export default Background
--- jeu-cv-react/src/page/Game.tsx
+++ jeu-cv-react/src/page/Game.tsx
@@ -4,28 +4,33 @@ import {useGameData} from "../store/GameProvider";
 import {ADD_PLAYER, ADD_DINO} from "../constants";
 import Hero from "../components/Hero";
 import {IPropsDino, createDinosaur, addDinosaurs} from "../components/Dinosaurs";
+import Background from "../components/Background";
 
 const Game = () => {
-    const [{player, dino}, dispatch] = useGameData()
-    const DinoMemoizedLength = ()=>useMemo(()=>dino.map((dinosaur:IPropsDino) => addDinosaurs(dinosaur)),[dino.length])
-    const newRef= useRef(createDinosaur())
-const [newDino, setNewDino] = useState(createDinosaur())
-    useEffect(()=>{
+    const xBackground = window.innerWidth / 4
+    const [{player, dino}, dispatch] = useGameData();
+    const FixedBackground = (compute:number)=> useMemo(()=><Background left={compute}/>, [xBackground])
+    const DinoMemoizedLength = () => useMemo(() => dino.map((dinosaur: IPropsDino) => addDinosaurs(dinosaur)), [dino.length])
+    const newRef = useRef(createDinosaur())
+    const [newDino, setNewDino] = useState(createDinosaur())
+    useEffect(() => {
         dispatch({type: ADD_PLAYER})
     }, [])
     useLayoutEffect(() => {
         newRef.current = createDinosaur()
         setNewDino(newRef.current)
-        let f =setInterval(() => {
+        let f = setInterval(() => {
             dispatch({type: ADD_DINO, newDino})
         }, 5000)
-        return ()=>clearInterval(f)
+        return () => clearInterval(f)
         console.log(dino)
     }, [dino.length])
     return (
         <>
-            {player && <MainHeader score={player.score} health={player.health} dynamite={player.dynamite}/>}
-
+            {FixedBackground (xBackground)}
+            {FixedBackground (xBackground * 2)}
+            {FixedBackground (xBackground * 3)}
+            {player && <MainHeader score={player.score} health={player.health} dynamite={player.dynamite} />}
             {player && <Hero />}
             {DinoMemoizedLength()}
         </>
