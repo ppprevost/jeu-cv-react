cddb34bcade0cc9d33100d9742dd577c05084750
Pierre-Philippe Prevost <ppprevost@aol.fr>
2020-01-05 18:49:22 +0100

    fix: prevent default for keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties, SyntheticEvent, useEffect, useRef} from "react";
+import React, { CSSProperties, SyntheticEvent, useEffect, useRef } from "react";
 import keyboardArrow from "../img/arrow-key.png";
 import styled from "styled-components";
 import { useGameData } from "../store/GameProvider";
@@ -19,6 +19,8 @@ const ContainerKeyboard = styled.div`
   right: 0%;
   bottom: 30px;
   margin: 0;
+  -webkit-user-select: none;
+  z-index: 5000;
   width: 100px;
   font-size: 2rem;
   text-align: center;
@@ -38,7 +40,7 @@ const keyboardTouch: CSSProperties = {
 };
 
 const Keyboard = () => {
-  const timeStamp = useRef(0)
+  const timeStamp = useRef(0);
 
   const [
     {
@@ -56,7 +58,7 @@ const Keyboard = () => {
     event.preventDefault();
     dispatch({ type: "SET_PAUSE" });
   };
-  const launchDynamite = (e: React.TouchEvent):void => {
+  const launchDynamite = (e: React.TouchEvent): void => {
     e.preventDefault();
     dispatch({ type: "IS_DYNAMITING" });
   };
@@ -64,55 +66,68 @@ const Keyboard = () => {
     e.preventDefault();
     dispatch({ type: MOVE_LEFT, stop });
   };
+  const moveRight = (e: React.TouchEvent, stop?: boolean) => {
+    e.preventDefault();
+    dispatch({ type: MOVE_RIGHT, stop });
+  };
+  const jump = (e: React.TouchEvent, stop?: boolean) => {
+    e.preventDefault();
+    dispatch({ type: JUMP, stop });
+  };
+  const crouch = (e: React.TouchEvent, stop?: boolean) => {
+    e.preventDefault();
+    dispatch({ type: IS_CROUCHING }, stop);
+  };
 
-useEffect(()=>{
-  window.addEventListener("touchstart", function(event_) {
-    if (event_.timeStamp - timeStamp.current < 300) { // A tap that occurs less than 300 ms from the last tap will trigger a double tap. This delay may be different between browsers.
-      event_.preventDefault();
-      return false;
-    }
-    timeStamp.current = event_.timeStamp;
-  })
-}, [])
+  useEffect(() => {
+    window.addEventListener("touchstart", function(event_) {
+      if (event_.timeStamp - timeStamp.current < 300) {
+        // A tap that occurs less than 300 ms from the last tap will trigger a double tap. This delay may be different between browsers.
+        event_.preventDefault();
+        return false;
+      }
+      timeStamp.current = event_.timeStamp;
+    });
+  }, []);
 
   return (
-    <>
-      <ContainerKeyboard>
-        <p className="no-margin" onTouchStart={setPause}>
-          P - Pause
-        </p>
-        <p className="no-margin" onTouchStart={launchDynamite}>
-          D - Dynamite
-        </p>
-        <p className="no-margin" onTouchStart={shoot}>
-          Space - Shoot
-        </p>
-      </ContainerKeyboard>
-      <ContainerArrow className="keyboard arrow">
-        <span>Use Arrow</span>
-        <div
-          onTouchStart={() => dispatch({ type: JUMP })}
-          onTouchEnd={() => dispatch({ type: JUMP, stop: true })}
-          style={{ ...keyboardTouch, left: "56px" }}
-        ></div>
-        <div
-          onTouchStart={moveLeft}
-          onTouchEnd={(event) => moveLeft(event, true)}
-          style={{ ...keyboardTouch, left: "7px", bottom: "5px" }}
-        ></div>
-        <div
-          onTouchStart={() => dispatch({ type: IS_CROUCHING })}
-          onTouchEnd={() => dispatch({ type: IS_CROUCHING, stop: true })}
-          style={{ ...keyboardTouch, left: "55px", bottom: "5px" }}
-        ></div>
-        <div
-          onTouchStart={() => dispatch({ type: MOVE_RIGHT })}
-          onTouchEnd={() => dispatch({ type: MOVE_RIGHT, stop: true })}
-          style={{ ...keyboardTouch, left: "103px", bottom: "5px" }}
-        ></div>
-        <KeyboardArrow src={keyboardArrow} alt="keyboardArrow" />
-      </ContainerArrow>
-    </>
+      <>
+        <ContainerKeyboard>
+          <p className="no-margin" onTouchStart={setPause}>
+            P - Pause
+          </p>
+          <p className="no-margin" onTouchStart={launchDynamite}>
+            D - Dynamite
+          </p>
+          <p className="no-margin" onTouchStart={shoot}>
+            Space - Shoot
+          </p>
+        </ContainerKeyboard>
+        <ContainerArrow className="keyboard arrow">
+          <span>Use Arrow</span>
+          <div
+              onTouchStart={jump}
+              onTouchEnd={e => jump(e, true)}
+              style={{ ...keyboardTouch, left: "56px" }}
+          ></div>
+          <div
+              onTouchStart={moveLeft}
+              onTouchEnd={event => moveLeft(event, true)}
+              style={{ ...keyboardTouch, left: "7px", bottom: "5px" }}
+          ></div>
+          <div
+              onTouchStart={crouch}
+              onTouchEnd={e => crouch(e, true)}
+              style={{ ...keyboardTouch, left: "55px", bottom: "5px" }}
+          ></div>
+          <div
+              onTouchStart={moveRight}
+              onTouchEnd={e => moveRight(e, true)}
+              style={{ ...keyboardTouch, left: "103px", bottom: "5px" }}
+          ></div>
+          <KeyboardArrow src={keyboardArrow} alt="keyboardArrow" />
+        </ContainerArrow>
+      </>
   );
 };
 
